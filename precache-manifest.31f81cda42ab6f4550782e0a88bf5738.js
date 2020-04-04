4938433983a44cb797e1feb583eda56eddd10efe
suchana <suchanachakraborty@gmail.com>
2020-04-04 19:51:44 +0530

    Updates

---
+++ precache-manifest.31f81cda42ab6f4550782e0a88bf5738.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf840f14d1ed66d0e2db527a8734b7c6",
+    "url": "/Animal-Farm/index.html"
+  },
+  {
+    "revision": "05713a57b2a66f649c75",
+    "url": "/Animal-Farm/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3a18fb1c80f2addc5a63",
+    "url": "/Animal-Farm/static/js/2.04ea09a4.chunk.js"
+  },
+  {
+    "revision": "c7efe547bd596498920ac96125899bf7",
+    "url": "/Animal-Farm/static/js/2.04ea09a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05713a57b2a66f649c75",
+    "url": "/Animal-Farm/static/js/main.cc9b0f81.chunk.js"
+  },
+  {
+    "revision": "53738cc2c9f848af46ba",
+    "url": "/Animal-Farm/static/js/runtime-main.cbdb48cf.js"
+  },
+  {
+    "revision": "73caaa01e4d700f4b23e47665ecc2727",
+    "url": "/Animal-Farm/static/media/facebook.73caaa01.svg"
+  },
+  {
+    "revision": "0b117993cfdc06f6a05973ffa37ab7dc",
+    "url": "/Animal-Farm/static/media/instagram.0b117993.svg"
+  },
+  {
+    "revision": "435fc4bc44c1e90d5f00f4efccdb9b1d",
+    "url": "/Animal-Farm/static/media/twitter.435fc4bc.svg"
+  }
+]);
